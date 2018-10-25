4826cd0091318f7b89bd50c35e1df82d6f3d7313
morsi-ext <maariano.orsi@gmail.com>
2018-10-25 02:32:08 -0300

    Global Error Handler

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -45,6 +45,8 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
+            throw new Exception("ERRORRRRR");
+
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
@@ -74,7 +76,7 @@ namespace DatingApp.API.Controllers
             return Ok(
                 new {
                         token = tokenHandler.WriteToken(token)
-                    });
+                    });            
         }
     }
 }
